d8bdb1241e326e1176a66cef1e040fc90b922830
cr34t1ve <cr34t1ve@protonmail.com>
2021-03-03 02:22:27 +0000

    'Enter Wallet Address' page constructed

--- lib/screens/enterwalletaddress/enterwalletaddress.dart
+++ lib/screens/enterwalletaddress/enterwalletaddress.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simplecoins_0/components/button.dart';
 import 'package:simplecoins_0/enum/constants.dart';
 
 class EnterWalletAddress extends StatefulWidget {
@@ -34,6 +35,48 @@ class _EnterWalletAddressState extends State<EnterWalletAddress> {
             ),
           )
         ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/Homescreen/$cryptoType.png',
+              width: 52.0,
+            ),
+            Spacer(
+              flex: 2,
+            ),
+            Row(
+              children: <Widget>[Text('Your Bitcoin Address')],
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            TextField(
+              autofocus: false,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                  //hintText: 'Email',
+                  contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0))),
+            ),
+            Spacer(),
+            Text(
+              'Click to paste address',
+              style: TextStyle(decoration: TextDecoration.underline),
+            ),
+            Spacer(
+              flex: 4,
+            ),
+            DefaultButton(
+              text: 'Next',
+              press: () {},
+            )
+          ],
+        ),
       ),
     );
   }
