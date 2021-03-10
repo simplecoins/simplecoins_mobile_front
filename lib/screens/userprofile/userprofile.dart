45f17503459fd0f517a6f964bb2ffe35e3d45cd2
cr34t1ve <cr34t1ve@protonmail.com>
2021-03-10 10:12:42 +0000

    'User Profile' styles and size constraints added

--- lib/screens/userprofile/userprofile.dart
+++ lib/screens/userprofile/userprofile.dart
@@ -45,10 +45,34 @@ class _UserProfileState extends State<UserProfile> {
             Row(
               children: [
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('GOOD EVENING'),
-                    Text('Andy Andy Kwesi'),
-                    Text('23 Total Orders')
+                    Text(
+                      'GOOD EVENING',
+                      style: TextStyle(
+                        fontFamily: 'Manrope',
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF808080)
+                      ),
+                    ),
+                    Text(
+                      'Andy Andy Kwesi',
+                      style: TextStyle(
+                        fontFamily: 'Manrope',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    Text(
+                      '23 Total Orders',
+                      style: TextStyle(
+                        fontFamily: 'Manrope',
+                        fontSize: 12.0,
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xFF40A187)
+                      ),
+                    )
                   ],
                 ),
                 Spacer(),
@@ -58,37 +82,121 @@ class _UserProfileState extends State<UserProfile> {
                 )
               ],
             ),
-            Row(
-              children: [Text('Personal info')],
+            SizedBox(
+              height: 56.0,
             ),
             Row(
               children: [
-                Text('E-mail'),
+                Text(
+                  'Personal info',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF243656)
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Row(
+              children: [
+                Text(
+                  'E-mail',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF243656)
+                  ),
+                ),
                 SizedBox(
                   width: 71.0,
                 ),
-                Text('vadikforz@gmail.com')
+                Text(
+                  'vadikforz@gmail.com',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF243656)
+                  ),
+                )
               ],
+            ),
+            SizedBox(
+              height: 18.0,
             ),
             Row(
               children: [
-                Text('Phone'),
+                Text(
+                  'Phone',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF243656)
+                  ),
+                ),
                 SizedBox(
                   width: 71.0,
                 ),
-                Text('+4 1782 049 294')
+                Text(
+                  '+4 1782 049 294',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF243656)
+                  ),
+                )
               ],
             ),
-            Row(
-              children: [Text('Security')],
+            SizedBox(
+              height: 40.0,
             ),
             Row(
               children: [
-                Text('Password'),
+                Text(
+                  'Security',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF243656)
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Row(
+              children: [
+                Text(
+                  'Password',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF243656)
+                  ),
+                ),
                 SizedBox(
                   width: 45.0,
                 ),
-                Text('··········'),
+                Text(
+                  '··········',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF243656)
+                  ),
+                ),
+                Spacer(),
                 IconButton(
                   icon: Icon(
                     Icons.edit,
@@ -100,57 +208,135 @@ class _UserProfileState extends State<UserProfile> {
             ),
             Row(
               children: [
-                Text('Identity'),
+                Text(
+                  'Identity',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF243656)
+                  ),
+                ),
                 SizedBox(
                   width: 60.0,
                 ),
-                Text('Verify my identity')
-              ],
-            ),
-            Row(
-              children: [Text('Support')],
-            ),
-            Row(
-              children: [
-                Text('Telegram'),
-                SizedBox(
-                  width: 71.0,
-                ),
                 Text(
-                  '@simplecoins',
+                  'Verify my identity',
                   style: TextStyle(
-                    decoration: TextDecoration.underline
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                    decoration: TextDecoration.underline,
+                    color: Color(0xFFAAABAE)
                   ),
                 )
               ],
             ),
+            SizedBox(
+              height: 40.0,
+            ),
             Row(
               children: [
-                Text('Twitter'),
-                SizedBox(
-                  width: 71.0,
-                ),
                 Text(
-                  '@simplecoins',
+                  'Support',
                   style: TextStyle(
-                    decoration: TextDecoration.underline
+                    fontFamily: 'Manrope',
+                    fontSize: 12.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF243656)
                   ),
                 )
               ],
             ),
+            SizedBox(
+              height: 16.0,
+            ),
             Row(
               children: [
-                Text('Email'),
+                Text(
+                  'Telegram',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF243656)
+                  ),
+                ),
                 SizedBox(
-                  width: 71.0,
+                  width: 48.0,
+                ),
+                Text(
+                  '@simplecoins',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF3E8BF7)
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 17.0,
+            ),
+            Row(
+              children: [
+                Text(
+                  'Twitter',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF243656)
+                  ),
+                ),
+                SizedBox(
+                  width: 65.0,
+                ),
+                Text(
+                  '@simplecoins',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF3E8BF7)
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 17.0,
+            ),
+            Row(
+              children: [
+                Text(
+                  'Email',
+                  style: TextStyle(
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF243656)
+                  ),
+                ),
+                SizedBox(
+                  width: 77.0,
                 ),
                 Text(
                   'support@simplecoins.africa',
                   style: TextStyle(
-                    decoration: TextDecoration.underline
+                    decoration: TextDecoration.underline,
+                    fontFamily: 'Manrope',
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFF3E8BF7)
                   ),
                 )
               ],
+            ),
+            SizedBox(
+              height: 69.0,
             ),
             SizedBox(
               height: 64,
