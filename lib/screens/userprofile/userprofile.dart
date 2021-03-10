2c7eb42c58c2b5dfa59d3003c68f36883213d39d
cr34t1ve <cr34t1ve@protonmail.com>
2021-03-10 08:56:18 +0000

    'User Profile' page constructed

--- lib/screens/userprofile/userprofile.dart
+++ lib/screens/userprofile/userprofile.dart
@@ -46,15 +46,9 @@ class _UserProfileState extends State<UserProfile> {
               children: [
                 Column(
                   children: [
-                    Text(
-                      'GOOD EVENING'
-                    ),
-                    Text(
-                      'Andy Andy Kwesi'
-                    ),
-                    Text(
-                      '23 Total Orders'
-                    )
+                    Text('GOOD EVENING'),
+                    Text('Andy Andy Kwesi'),
+                    Text('23 Total Orders')
                   ],
                 ),
                 Spacer(),
@@ -63,6 +57,124 @@ class _UserProfileState extends State<UserProfile> {
                   width: 93.0,
                 )
               ],
+            ),
+            Row(
+              children: [Text('Personal info')],
+            ),
+            Row(
+              children: [
+                Text('E-mail'),
+                SizedBox(
+                  width: 71.0,
+                ),
+                Text('vadikforz@gmail.com')
+              ],
+            ),
+            Row(
+              children: [
+                Text('Phone'),
+                SizedBox(
+                  width: 71.0,
+                ),
+                Text('+4 1782 049 294')
+              ],
+            ),
+            Row(
+              children: [Text('Security')],
+            ),
+            Row(
+              children: [
+                Text('Password'),
+                SizedBox(
+                  width: 45.0,
+                ),
+                Text('··········'),
+                IconButton(
+                  icon: Icon(
+                    Icons.edit,
+                    color: Color(0xFF3E8BF7),
+                  ),
+                  onPressed: () {},
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Text('Identity'),
+                SizedBox(
+                  width: 60.0,
+                ),
+                Text('Verify my identity')
+              ],
+            ),
+            Row(
+              children: [Text('Support')],
+            ),
+            Row(
+              children: [
+                Text('Telegram'),
+                SizedBox(
+                  width: 71.0,
+                ),
+                Text(
+                  '@simplecoins',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Text('Twitter'),
+                SizedBox(
+                  width: 71.0,
+                ),
+                Text(
+                  '@simplecoins',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline
+                  ),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Text('Email'),
+                SizedBox(
+                  width: 71.0,
+                ),
+                Text(
+                  'support@simplecoins.africa',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 64,
+              width: 311,
+              child: FlatButton(
+                padding: EdgeInsets.all(20.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    side: BorderSide(color: Color(0xFFE56565))),
+                //color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Logout',
+                      style: TextStyle(
+                        color: Color(0xFFE56565),
+                        fontFamily: 'Manrope'
+                        ),
+                    ),
+                  ],
+                ),
+                onPressed: () {},
+              ),
             )
           ],
         ),
