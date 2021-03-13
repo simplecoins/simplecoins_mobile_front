b4a7fc788aa35b0e63e8a2f5a4bf59e95c9c7585
cr34t1ve <cr34t1ve@protonmail.com>
2021-03-13 13:52:17 +0000

    Discard Alert Dialog constructed

--- lib/screens/paymentinfo/paymentinfo.dart
+++ lib/screens/paymentinfo/paymentinfo.dart
@@ -152,11 +152,41 @@ class _PaymentInfoState extends State<PaymentInfo> {
 
 // set up the buttons
 Widget discardButton = FlatButton(
-  child: Text("Remind me later"),
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0)
+    )
+  ),
+  padding: EdgeInsets.only(left: 36.0, right: 36.0, top: 16.0, bottom: 16.0),
+  color: Colors.white,
+  child: Text(
+    "Yes, Discard it",
+    style: TextStyle(
+      color: Color(0xFFFC8181),
+      fontFamily: 'Manrope',
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+    ),
+  ),
   onPressed: () {},
 );
 Widget cancelButton = FlatButton(
-  child: Text("Cancel"),
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0)
+    )
+  ),
+  padding: EdgeInsets.only(left: 40.0, right: 40.0, top: 16.0, bottom: 16.0),
+  color: Color(0xFF2F7CF6),
+  child: Text(
+    "Cancel",
+    style: TextStyle(
+      color: Colors.white,
+      fontFamily: 'Manrope',
+      fontSize: 16,
+      fontWeight: FontWeight.w400,
+    ),
+  ),
   onPressed: () {},
 );
 // Widget launchButton = FlatButton(
@@ -166,7 +196,15 @@ Widget cancelButton = FlatButton(
 
 // set up the AlertDialog
 AlertDialog alert = AlertDialog(
-  contentPadding: EdgeInsets.all(60.0),
+  insetPadding: EdgeInsets.all(20.0),
+  backgroundColor: Colors.white,
+  contentPadding: EdgeInsets.only(top: 60.0, bottom: 46.0),
+  actionsPadding: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 38.0),
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(10.0)
+    )
+  ),
   content: Text(
       "Do you wish to discard\nthis order?",
       style: TextStyle(
