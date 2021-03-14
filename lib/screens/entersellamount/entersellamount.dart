78426a8efb0eb28c5a93d3d336afc500a52eae8a
cr34t1ve <cr34t1ve@protonmail.com>
2021-03-14 00:44:50 +0000

    'Enter Sell Amount' page initialised

---
+++ lib/screens/entersellamount/entersellamount.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EnterSellAmount extends StatefulWidget {
+  @override
+  _EnterSellAmountState createState() => _EnterSellAmountState();
+}
+
+class _EnterSellAmountState extends State<EnterSellAmount> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
