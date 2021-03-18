278f335de5451813f38dff0379818dd4c63a8134
cr34t1ve <cr34t1ve@protonmail.com>
2021-03-18 12:30:17 +0000

    'Coin Payment' page initialised

---
+++ lib/screens/coinpayment/coinpayment.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CoinPayment extends StatefulWidget {
+  @override
+  _CoinPaymentState createState() => _CoinPaymentState();
+}
+
+class _CoinPaymentState extends State<CoinPayment> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        
+      ),
+    );
+  }
+}
