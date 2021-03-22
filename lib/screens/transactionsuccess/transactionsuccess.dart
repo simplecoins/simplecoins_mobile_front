6955586704d18909a1a71fa272c6689c13d64300
cr34t1ve <cr34t1ve@protonmail.com>
2021-03-22 15:36:33 +0000

    'Transaction Success' page constructed

--- lib/screens/transactionsuccess/transactionsuccess.dart
+++ lib/screens/transactionsuccess/transactionsuccess.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simplecoins_0/components/button.dart';
 
 class TransactionSuccess extends StatefulWidget {
   @override
@@ -30,6 +31,9 @@ class _TransactionSuccessState extends State<TransactionSuccess> {
         padding: EdgeInsets.all(12.0),
         child: Column(
           children: [
+            SizedBox(
+              height: 151.0,
+            ),
             Row(
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
@@ -46,26 +50,73 @@ class _TransactionSuccessState extends State<TransactionSuccess> {
                 // Note: Styles for TextSpans must be explicitly defined.
                 // Child text spans will inherit styles from parent
                 style: TextStyle(
-                fontFamily: 'Manrope',
-                fontWeight: FontWeight.w400,
-                fontSize: 18,
-                color: Colors.black
-              ),
+                    fontFamily: 'Manrope',
+                    fontWeight: FontWeight.w400,
+                    fontSize: 18,
+                    color: Colors.black),
                 children: <TextSpan>[
-                  TextSpan(text: 'Awesome!!\nYou just placed an order to Buy '),
                   TextSpan(
-                      text: 'GHS\n 738.00 ',
+                      text: 'Awesome!!\nYou just placed an order to Buy\n'),
+                  TextSpan(
+                      text: 'GHS 738.00 ',
                       style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontFamily: 'Manrope',
-                        fontWeight: FontWeight.w600
-                      )
-                    ),
+                          color: Colors.black,
+                          fontSize: 18,
+                          fontFamily: 'Manrope',
+                          fontWeight: FontWeight.w600)),
                   TextSpan(text: 'worth of Bitcoin.'),
                 ],
               ),
             ),
+            SizedBox(
+              height: 41.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'We are now ',
+                  style: TextStyle(
+                      fontFamily: 'Manrope',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                      color: Colors.black),
+                ),
+                Container(
+                  width: 122.0,
+                  height: 27,
+                  decoration: BoxDecoration(
+                      color: Color(0xFFDCE1FF),
+                      borderRadius: BorderRadius.circular(52.0)),
+                  child: Center(
+                    child: Text(
+                      'PROCESSING',
+                      style: TextStyle(
+                          fontFamily: 'Manrope',
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF4F49C1)),
+                    ),
+                  ),
+                ),
+                Text(
+                  ' your order',
+                  style: TextStyle(
+                      fontFamily: 'Manrope',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                      color: Colors.black),
+                ),
+              ],
+            ),
+            Spacer(),
+            DefaultButton(
+              text: 'View my transactions',
+              press: () {},
+            ),
+            SizedBox(
+              height: 15.0,
+            )
           ],
         ),
       ),
