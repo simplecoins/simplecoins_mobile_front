0f62c96be0336ccd4639a788ecfd972e3c89b227
cr34t1ve <cr34t1ve@protonmail.com>
2021-03-22 18:18:02 +0000

    'Sign Up' sizing and constraints improved
    spacing changed from spacers to sized boxes. Single Child Scroll View added

--- lib/screens/signup/signup.dart
+++ lib/screens/signup/signup.dart
@@ -39,153 +39,159 @@ class _SignUpState extends State<SignUp> {
       ),
       body: Padding(
         padding: EdgeInsets.all(23.0),
-        child: Column(
-          //crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Spacer(
-              flex: 3,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Create an account',
-                  style: TextStyle(
-                      fontFamily: "Manrope",
-                      fontSize: 24,
-                      fontWeight: FontWeight.w800),
-                ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Sign Up with your details to \ncontinue',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      color: Color(0xFF787879),
-                      fontFamily: "Manrope",
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-            Spacer(
-              flex: 4,
-            ),
-            TextField(
-              autofocus: false,
-              keyboardType: TextInputType.name,
-              decoration: InputDecoration(
-                  hintText: 'Full Name',
-                  contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0))),
-            ),
-            Spacer(),
-            TextField(
-              autofocus: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  hintText: 'Email',
-                  contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0))),
-            ),
-            Spacer(),
-            TextFormField(
-              controller: _pController,
-              onSaved: (val) => _password = val,
-              autofocus: false,
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: _isHidden,
-              decoration: InputDecoration(
-                  focusColor: Colors.black,
-                  suffixIcon: GestureDetector(
-                    onTap: _togglePass,
-                    child: Icon(
-                      Icons.remove_red_eye_outlined,
-                      //color: Colors.black,
-                    ),
-                  ),
-                  hintText: 'Password',
-                  contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0))),
-            ),
-            Spacer(),
-            TextFormField(
-              controller: _cpController,
-              onSaved: (val) => _password = val,
-              autofocus: false,
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: _isHidden,
-              decoration: InputDecoration(
-                  focusColor: Colors.black,
-                  suffixIcon: GestureDetector(
-                    onTap: _togglePass,
-                    child: Icon(
-                      Icons.remove_red_eye_outlined,
-                      //color: Colors.black,
-                    ),
-                  ),
-                  hintText: 'Confirm Password',
-                  contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0))),
-            ),
-            Spacer(),
-            Row(
-              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Checkbox(
-                  activeColor: Colors.black,
-                  value: _isChecked,
-                  onChanged: (value) {
-                    setState(() {
-                      _isChecked = !_isChecked;
-                    });
-                  },
-                ),
-                Text(
-                  'I certify that I am 18 years of age or older, and\nagree to the User Agreement and Privacy Policy.',
-                  style: TextStyle(
-                      fontFamily: 'Manrope',
-                      fontWeight: FontWeight.w400,
-                      fontSize: 12,
-                      color: Color(0xFF787879)),
-                )
-              ],
-            ),
-            Spacer(
-              flex: 3,
-            ),
-            DefaultButton(
-              text: 'Sign Up',
-              press: () {
-                _pController.text == _cpController.text
-                    ? print('yes')
-                    : print('no');
-              },
-            ),
-            Spacer(
-              flex: 2,
-            ),
-            Spacer(
-              flex: 4,
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: Text(
-                "I already have an account",
-                style: TextStyle(decoration: TextDecoration.underline),
+        child: SingleChildScrollView(
+          child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 34.0,
               ),
-            ),
-            Spacer(
-              flex: 3,
-            )
-          ],
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Create an account',
+                    style: TextStyle(
+                        fontFamily: "Manrope",
+                        fontSize: 24,
+                        fontWeight: FontWeight.w800),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Sign Up with your details to \ncontinue',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Color(0xFF787879),
+                        fontFamily: "Manrope",
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextField(
+                autofocus: false,
+                keyboardType: TextInputType.name,
+                decoration: InputDecoration(
+                    hintText: 'Full Name',
+                    contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextField(
+                autofocus: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    hintText: 'Email',
+                    contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                controller: _pController,
+                onSaved: (val) => _password = val,
+                autofocus: false,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: _isHidden,
+                decoration: InputDecoration(
+                    focusColor: Colors.black,
+                    suffixIcon: GestureDetector(
+                      onTap: _togglePass,
+                      child: Icon(
+                        Icons.remove_red_eye_outlined,
+                        //color: Colors.black,
+                      ),
+                    ),
+                    hintText: 'Password',
+                    contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                controller: _cpController,
+                onSaved: (val) => _password = val,
+                autofocus: false,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: _isHidden,
+                decoration: InputDecoration(
+                    focusColor: Colors.black,
+                    suffixIcon: GestureDetector(
+                      onTap: _togglePass,
+                      child: Icon(
+                        Icons.remove_red_eye_outlined,
+                        //color: Colors.black,
+                      ),
+                    ),
+                    hintText: 'Confirm Password',
+                    contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Checkbox(
+                    activeColor: Colors.black,
+                    value: _isChecked,
+                    onChanged: (value) {
+                      setState(() {
+                        _isChecked = !_isChecked;
+                      });
+                    },
+                  ),
+                  Text(
+                    'I certify that I am 18 years of age or older, and\nagree to the User Agreement and Privacy Policy.',
+                    style: TextStyle(
+                        fontFamily: 'Manrope',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12,
+                        color: Color(0xFF787879)),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              DefaultButton(
+                text: 'Sign Up',
+                press: () {
+                  _pController.text == _cpController.text
+                      ? print('yes')
+                      : print('no');
+                },
+                bcolor: Colors.black,
+                tcolor: Colors.white,
+              ),
+              SizedBox(
+                height: 36.0,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  "I already have an account",
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
