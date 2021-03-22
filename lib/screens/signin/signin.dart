331c79997166a541e8568731e23148b6385f6929
cr34t1ve <cr34t1ve@protonmail.com>
2021-03-22 18:10:21 +0000

    'Sign In' page overflow issue fixed

--- lib/screens/signin/signin.dart
+++ lib/screens/signin/signin.dart
@@ -31,101 +31,107 @@ class _SignInState extends State<SignIn> {
       ),
       body: Padding(
         padding: EdgeInsets.all(23.0),
-        child: Column(
-          //crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Spacer(
-              flex: 3,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Welcome',
-                  style: TextStyle(
-                      fontFamily: "Manrope",
-                      fontSize: 24,
-                      fontWeight: FontWeight.w800),
-                ),
-              ],
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Sign In with your details to \ncontinue',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      color: Color(0xFF787879),
-                      fontFamily: "Manrope",
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500),
-                ),
-              ],
-            ),
-            Spacer(
-              flex: 4,
-            ),
-            TextField(
-              autofocus: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  hintText: 'Email',
-                  contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0))),
-            ),
-            Spacer(),
-            TextFormField(
-              onSaved: (val) => _password = val,
-              autofocus: false,
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: _isHidden,
-              decoration: InputDecoration(
-                  focusColor: Colors.black,
-                  suffixIcon: GestureDetector(
-                    onTap: _toggle,
-                    child: Icon(
-                      Icons.remove_red_eye_outlined,
-                      //color: Colors.black,
-                    ),
-                  ),
-                  hintText: 'Password',
-                  contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0))),
-            ),
-            Spacer(
-              flex: 3,
-            ),
-            DefaultButton(
-              text: 'Sign In',
-              press: () {},
-              bcolor: Colors.black,
-              tcolor: Colors.white,
-            ),
-            Spacer(
-              flex: 2,
-            ),
-            Text('OR'),
-            Spacer(
-              flex: 2,
-            ),
-            GoogleButton(),
-            Spacer(
-              flex: 4,
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: Text(
-                "I don't have an account",
-                style: TextStyle(decoration: TextDecoration.underline),
+        child: SingleChildScrollView(
+          child: Column(
+            //crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 55.0,
               ),
-            ),
-            Spacer(
-              flex: 3,
-            )
-          ],
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Welcome',
+                    style: TextStyle(
+                        fontFamily: "Manrope",
+                        fontSize: 24,
+                        fontWeight: FontWeight.w800),
+                  ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Sign In with your details to \ncontinue',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Color(0xFF787879),
+                        fontFamily: "Manrope",
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 60.0,
+              ),
+              TextField(
+                autofocus: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    hintText: 'Email',
+                    contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                onSaved: (val) => _password = val,
+                autofocus: false,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: _isHidden,
+                decoration: InputDecoration(
+                    focusColor: Colors.black,
+                    suffixIcon: GestureDetector(
+                      onTap: _toggle,
+                      child: Icon(
+                        Icons.remove_red_eye_outlined,
+                        //color: Colors.black,
+                      ),
+                    ),
+                    hintText: 'Password',
+                    contentPadding: EdgeInsets.fromLTRB(20, 25, 20, 25),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              DefaultButton(
+                text: 'Sign In',
+                press: () {},
+                bcolor: Colors.black,
+                tcolor: Colors.white,
+              ),
+              SizedBox(
+                height: 26.0,
+              ),
+              Text('OR'),
+              SizedBox(
+                height: 37.0,
+              ),
+              GoogleButton(),
+              SizedBox(
+                height: 56.0,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  "I don't have an account",
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    fontFamily: 'Manrope',
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
