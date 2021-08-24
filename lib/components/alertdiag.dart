fcfad534ba885f1956883eb13d25690b5f92944f
Master Sofua Desmond <mrsofua@gmail.com>
2021-08-24 21:17:41 +0000

    added dynamic sizing to alert dialog

--- lib/components/alertdiag.dart
+++ lib/components/alertdiag.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simplecoins_0/utils/sizeConfig.dart';
 // import 'package:flutter/scheduler.dart';
 
 showAlertDialog(BuildContext context) {
@@ -6,14 +7,18 @@ showAlertDialog(BuildContext context) {
   Widget discardButton = FlatButton(
     shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(10.0))),
-    padding: EdgeInsets.only(left: 34.0, right: 34.0, top: 16.0, bottom: 16.0),
+    padding: EdgeInsets.only(
+        left: getProportionateScreenWidth(34.0),
+        right: getProportionateScreenWidth(34.0),
+        top: getProportionateScreenHeight(16.0),
+        bottom: getProportionateScreenHeight(16.0)),
     color: Colors.white,
     child: Text(
       "Yes, Discard it",
       style: TextStyle(
         color: Color(0xFFFC8181),
         fontFamily: 'Manrope',
-        fontSize: 16,
+        fontSize: getProportionateScreenWidth(16),
         fontWeight: FontWeight.w400,
       ),
     ),
@@ -28,14 +33,18 @@ showAlertDialog(BuildContext context) {
   Widget cancelButton = FlatButton(
     shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(10.0))),
-    padding: EdgeInsets.only(left: 38.0, right: 38.0, top: 16.0, bottom: 16.0),
+    padding: EdgeInsets.only(
+        left: getProportionateScreenWidth(36.0),
+        right: getProportionateScreenWidth(36.0),
+        top: getProportionateScreenHeight(16.0),
+        bottom: getProportionateScreenHeight(16.0)),
     color: Color(0xFF2F7CF6),
     child: Text(
       "Cancel",
       style: TextStyle(
         color: Colors.white,
         fontFamily: 'Manrope',
-        fontSize: 16,
+        fontSize: getProportionateScreenWidth(16),
         fontWeight: FontWeight.w400,
       ),
     ),
@@ -46,17 +55,22 @@ showAlertDialog(BuildContext context) {
 
 // set up the AlertDialog
   AlertDialog alert = AlertDialog(
-    insetPadding: EdgeInsets.all(20.0),
+    insetPadding: EdgeInsets.all(getProportionateScreenWidth(20.0)),
     backgroundColor: Colors.white,
-    contentPadding: EdgeInsets.only(top: 60.0, bottom: 46.0),
-    actionsPadding: EdgeInsets.only(left: 19.0, right: 19.0, bottom: 38.0),
+    contentPadding: EdgeInsets.only(
+        top: getProportionateScreenHeight(60.0),
+        bottom: getProportionateScreenHeight(46.0)),
+    actionsPadding: EdgeInsets.only(
+        left: getProportionateScreenWidth(19.0),
+        right: getProportionateScreenWidth(19.0),
+        bottom: getProportionateScreenHeight(38.0)),
     shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.all(Radius.circular(10.0))),
     content: Text(
       "Do you wish to discard\nthis order?",
       style: TextStyle(
           fontFamily: 'Manrope',
-          fontSize: 20,
+          fontSize: getProportionateScreenWidth(20),
           fontWeight: FontWeight.w400,
           color: Color(0xFF243656)),
       textAlign: TextAlign.center,
