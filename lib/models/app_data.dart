8a63093f6a20a128d57c36e6f309131828fa6ca1
cr34t1ve <mrsofua@gmail.com>
2022-03-19 17:01:31 +0000

    Create app_data.dart

---
+++ lib/models/app_data.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppData extends ChangeNotifier {
+  List currencies = ['Bitcoin', 'USDT', 'Ripple'];
+  List status = ['AWAITING PAYMENT', 'PROCESSING', 'PROCESSED', 'CANCELLED'];
+
+  double _buyRate = 5.70;
+  double _btcRate = 0.000017;
+
+  AppData({this.currencies, this.status});
+}
